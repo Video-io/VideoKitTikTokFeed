fa4f94eec3df49de260f06950ae056f4df80d126
Dennis Stücken <dstuecken@me.com>
2020-11-11 10:29:41 -0800

    Initial Commit

---
+++ VideoKitCustomRecorder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoKitCustomRecorder
+//
+//  Created by Dennis Stücken on 11/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
